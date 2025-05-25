183f693b73d7591c579856c83dbe7682ec746d29
manoj-gowda-369 <manoj.g@s.amity.edu>
2025-05-25 23:10:13 +0530

    working

--- naveen.py
+++ naveen.py
@@ -4,3 +4,7 @@ def get_git_user_info():
     try:
         # Get local Git user name
         name = subprocess.check_output(['git', 'config', 'user.name']).decode().strip()
+         # Get local Git user email
+        email = subprocess.check_output(['git', 'config', 'user.email']).decode().strip()
+
+        print("✅ Current Git user info (local repo):")
